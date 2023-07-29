84c533b298827ae000dcdf8e0f45733a852f282c
@nain333 <hnain70@gmail.com>
2023-07-30 04:02:58 +0530

    Listen on a port

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express();
+const port = 7000
+app.listen(port,(err)=>{
+    if(err){
+        console.log('Error in running the server',err)
+    }
+    console.log(`server is ua and running on port: ${port}`)
+})
