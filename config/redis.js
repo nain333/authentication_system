229f42056061164f0ed8294634c103449fb147e1
@nain333 <hnain70@gmail.com>
2023-08-19 04:01:33 +0530

    fix the redis connection config

--- config/redis.js
+++ config/redis.js
@@ -1,11 +1,21 @@
-const redis=require('redis')
-const createClient=redis.createClient
+const redis = require('redis');
 
-const client = createClient({
-    url:'redis://red-cjds7cavvtos73bpvvn0:6379'
+// Redis configuration options
+const redisConfig = {
+  url: 'redis://red-cjds7cavvtos73bpvvn0:6379'
+};
+
+// Create a Redis client
+const client = redis.createClient(redisConfig);
+
+// Event listeners for Redis client
+client.on('connect', () => {
+  console.log('Connected to Redis');
 });
 
-client.on('error', err => console.log('Redis Client Error', err));
+client.on('error', (error) => {
+  console.error('Error connecting to Redis:', error);
+});
 
-const redisConnection=client.connect();
-module.exports=redisConnection
+// Export the Redis client
+module.exports = client;
