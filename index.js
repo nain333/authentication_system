8dca95ab9edfa829f52f905bddc4cba0f9e52a50
@nain333 <hnain70@gmail.com>
2023-08-21 03:37:48 +0530

    hide session secret in process.env

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use(express.urlencoded({extended:true}))
 app.use(expressSession(
     {
         name:'Authenticator',
-        secret:'create a session buddy',
+        secret:process.env.sessionSecret,
         saveUninitialized:false,
         resave:false,
         cookie:{
