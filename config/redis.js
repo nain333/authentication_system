7e9f7a122dbf49090599858708a51d690a0c3d04
@nain333 <hnain70@gmail.com>
2023-08-21 00:47:51 +0530

    re-add the start script and change redis config

--- config/redis.js
+++ config/redis.js
@@ -1,23 +1,77 @@
+// // const redis = require('redis');
+
+// // // Redis configuration options
+// // const redisConfig = {
+// //   url: 'rediss://red-cjds7cavvtos73bpvvn0:8ALjoxstNZgb9CotNIAQpxr45IYWdMBP@singapore-redis.render.com:6379',
+  
+// // };
+// // console.log('redis config started with redis url: ',redisConfig.url)
+// // // Create a Redis client
+// // const client = redis.createClient(redisConfig);
+// // console.log("client added: ",client)
+// // // Event listeners for Redis client
+// // client.once('connect', () => {
+// //   console.log('Connected to Redis');
+// // });
+
+// // client.on('error', (error) => {
+// //   console.error('Error connecting to Redis:', error);
+// // });
+// // console.log('exporting the connection')
+
+// // // Export the Redis client
+// // module.exports = client;
+// const redis = require('redis');
+
+// // Redis configuration options
+// const redisConfig = {
+//   url: 'rediss://red-cjds7cavvtos73bpvvn0:8ALjoxstNZgb9CotNIAQpxr45IYWdMBP@singapore-redis.render.com:6379',
+//   host: 'red-cjds7cavvtos73bpvvn0'
+// };
+// console.log('redis config started with redis url: ',redisConfig.url)
+
+// // Create a Redis client
+// const client = redis.createClient(redisConfig);
+// console.log("client added: ",client)
+
+// // Event listeners for Redis client
+// client.once('connect', () => {
+//   console.log('Connected to Redis');
+// });
+
+// client.on('error', (error) => {
+//   console.error('Error connecting to Redis:', error);
+// });
+
+// console.log('exporting the connection')
+
+// // Export the Redis client
+// module.exports = client;
+
+// // Add the async function to connect
+// (async () => {
+//   await client.connect();
+// })();
 const redis = require('redis');
 
 // Redis configuration options
-const redisConfig = {
-  url: 'rediss://red-cjds7cavvtos73bpvvn0:8ALjoxstNZgb9CotNIAQpxr45IYWdMBP@singapore-redis.render.com:6379',
-  host: 'red-cjds7cavvtos73bpvvn0'
+// const redisURL = 'rediss://red-cjds7cavvtos73bpvvn0:8ALjoxstNZgb9CotNIAQpxr45IYWdMBP@singapore-redis.render.com:6379';
+const redisURL = 'redis://red-cjds7cavvtos73bpvvn0:6379'
+const redisOptions = {
+  url: redisURL,
 };
-console.log('redis config started with redis url: ',redisConfig.url)
-// Create a Redis client
-const client = redis.createClient(redisConfig);
-console.log("client added: ",client)
+
+// Create a Redis client using the URL as the configuration
+const client = redis.createClient(redisOptions);
+
 // Event listeners for Redis client
-client.on('connect', () => {
+client.once('connect', () => {
   console.log('Connected to Redis');
 });
 
 client.on('error', (error) => {
   console.error('Error connecting to Redis:', error);
 });
-console.log('exporting the connection')
 
 // Export the Redis client
 module.exports = client;
