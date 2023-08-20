d32b8af7861e63edc55539b325365d2f937faa3e
@nain333 <hnain70@gmail.com>
2023-08-21 04:21:02 +0530

    update mongoose connection url

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const mongoose = require('mongoose')
 const dburl=process.env.mongoUrl
 // connect mongoose to the database
-mongoose.connect('mongodb://127.0.0.1:27017/authentication-system')
+mongoose.connect(process.env.mongoUrl)
 const db = mongoose.connection
 db.on('error',console.error.bind(console,`Error while connecting to db`))
 db.once('open',()=>{
