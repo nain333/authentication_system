2354672b8013583f5a7fdd53b97d9ddf919eb4f3
@nain333 <hnain70@gmail.com>
2023-08-22 02:55:30 +0530

    improove the frontEnd

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -2,5 +2,5 @@ const express = require('express')
 const passport = require('passport')
 const dashboardController=require('../controllers/dashboard_controller')
 const router = express.Router()
-router.get('/',passport.checkAuthentication,dashboardController.dashboard)
+router.get('/:id',passport.checkAuthentication,dashboardController.dashboard)
 module.exports=router
